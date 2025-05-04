2fcff7ff0988af8250aa5fbcb69d2a58a9903c99
Anna <annamurray278@gmail.com>
2025-05-04 10:45:28 -0500

    helpers for prev

--- hw3/hw3/hw3.c
+++ hw3/hw3/hw3.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/adc.h"
 
 #define LED_PIN 15
 #define BUTTON_PIN 2
 
-int main()
+void init_gpios()
+{
+    gpio_init(15);
+    gpio_init(2);
+
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+}
+
+void init_usb_port()
 {
     stdio_init_all();
     while (!stdio_usb_connected())
@@ -12,25 +22,30 @@ int main()
         sleep_ms(100);
     }
     printf("Start!\n");
+}
 
-    gpio_init(15);
-    gpio_init(2);
+void poll_button()
+{
+    if (gpio_get(BUTTON_PIN))
+    {
+        gpio_put(LED_PIN, 1);
+        printf("LED OFF\n");
+    }
+    else
+    {
+        gpio_put(LED_PIN, 0);
+        printf("LED ON\n");
+    }
+}
 
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+int main()
+{
+    init_usb_port();
+    init_gpios();
 
     while (1)
     {
-        if (gpio_get(BUTTON_PIN))
-        {
-            gpio_put(LED_PIN, 1);
-            printf("LED OFF\n");
-        }
-        else
-        {
-            gpio_put(LED_PIN, 0);
-            printf("LED ON\n");
-        }
+        poll_button();
         // char message[100];
         // scanf("%s", message);
         // printf("message: %s\r\n", message);
