ae49bceb1e18e0f7c798eb0b26cffef5294c7dc9
Anna <annamurray278@gmail.com>
2025-05-04 10:42:26 -0500

    led and button works

--- hw3/hw3/hw3.c
+++ hw3/hw3/hw3.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define LED_PIN 15
+#define BUTTON_PIN 2
+
 int main()
 {
     stdio_init_all();
@@ -10,11 +13,27 @@ int main()
     }
     printf("Start!\n");
 
+    gpio_init(15);
+    gpio_init(2);
+
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+
     while (1)
     {
-        char message[100];
-        scanf("%s", message);
-        printf("message: %s\r\n", message);
-        sleep_ms(50);
+        if (gpio_get(BUTTON_PIN))
+        {
+            gpio_put(LED_PIN, 1);
+            printf("LED OFF\n");
+        }
+        else
+        {
+            gpio_put(LED_PIN, 0);
+            printf("LED ON\n");
+        }
+        // char message[100];
+        // scanf("%s", message);
+        // printf("message: %s\r\n", message);
+        // sleep_ms(50);
     }
 }
