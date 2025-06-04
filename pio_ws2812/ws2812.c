1a1eed78f72d977265d6abd94b447068030ac3c1
Anna <annamurray278@gmail.com>
2025-06-03 21:51:38 -0500

    hw8 done

--- pio_ws2812/ws2812.c
+++ pio_ws2812/ws2812.c
@@ -5,6 +5,7 @@
  */
 
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 
 #include "pico/stdlib.h"
@@ -16,7 +17,7 @@
  * NOTE:
  *  Take into consideration if your WS2812 is a RGB or RGBW variant.
  *
- *  If it is RGBW, you need to set IS_RGBW to true and provide 4 bytes per 
+ *  If it is RGBW, you need to set IS_RGBW to true and provide 4 bytes per
  *  pixel (Red, Green, Blue, White) and use urgbw_u32().
  *
  *  If it is RGB, set IS_RGBW to false and provide 3 bytes per pixel (Red,
@@ -26,7 +27,7 @@
  *
  */
 #define IS_RGBW false
-#define NUM_PIXELS 150
+#define NUM_PIXELS 4
 
 #ifdef PICO_DEFAULT_WS2812_PIN
 #define WS2812_PIN PICO_DEFAULT_WS2812_PIN
@@ -40,75 +41,118 @@
 #error Attempting to use a pin>=32 on a platform that does not support it
 #endif
 
-static inline void put_pixel(PIO pio, uint sm, uint32_t pixel_grb) {
+static inline void put_pixel(PIO pio, uint sm, uint32_t pixel_grb)
+{
     pio_sm_put_blocking(pio, sm, pixel_grb << 8u);
 }
 
-static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
-    return
-            ((uint32_t) (r) << 8) |
-            ((uint32_t) (g) << 16) |
-            (uint32_t) (b);
+static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b)
+{
+    return ((uint32_t)(g) << 8) |
+           ((uint32_t)(r) << 16) |
+           (uint32_t)(b);
 }
 
-static inline uint32_t urgbw_u32(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
-    return
-            ((uint32_t) (r) << 8) |
-            ((uint32_t) (g) << 16) |
-            ((uint32_t) (w) << 24) |
-            (uint32_t) (b);
-}
+// static inline uint32_t urgbw_u32(uint8_t r, uint8_t g, uint8_t b, uint8_t w)
+// {
+//     return ((uint32_t)(r) << 8) |
+//            ((uint32_t)(g) << 16) |
+//            ((uint32_t)(w) << 24) |
+//            (uint32_t)(b);
+// }
 
-void pattern_snakes(PIO pio, uint sm, uint len, uint t) {
-    for (uint i = 0; i < len; ++i) {
-        uint x = (i + (t >> 1)) % 64;
-        if (x < 10)
-            put_pixel(pio, sm, urgb_u32(0xff, 0, 0));
-        else if (x >= 15 && x < 25)
-            put_pixel(pio, sm, urgb_u32(0, 0xff, 0));
-        else if (x >= 30 && x < 40)
-            put_pixel(pio, sm, urgb_u32(0, 0, 0xff));
-        else
-            put_pixel(pio, sm, 0);
+// link three 8bit colors together
+typedef struct
+{
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+} wsColor;
+
+// adapted from https://forum.arduino.cc/index.php?topic=8498.0
+// hue is a number from 0 to 360 that describes a color on the color wheel
+// sat is the saturation level, from 0 to 1, where 1 is full color and 0 is gray
+// brightness sets the maximum brightness, from 0 to 1
+wsColor HSBtoRGB(float hue, float sat, float brightness)
+{
+    float red = 0.0;
+    float green = 0.0;
+    float blue = 0.0;
+
+    if (sat == 0.0)
+    {
+        red = brightness;
+        green = brightness;
+        blue = brightness;
     }
-}
+    else
+    {
+        if (hue == 360.0)
+        {
+            hue = 0;
+        }
 
-void pattern_random(PIO pio, uint sm, uint len, uint t) {
-    if (t % 8)
-        return;
-    for (uint i = 0; i < len; ++i)
-        put_pixel(pio, sm, rand());
-}
+        int slice = hue / 60.0;
+        float hue_frac = (hue / 60.0) - slice;
 
-void pattern_sparkle(PIO pio, uint sm, uint len, uint t) {
-    if (t % 8)
-        return;
-    for (uint i = 0; i < len; ++i)
-        put_pixel(pio, sm, rand() % 16 ? 0 : 0xffffffff);
-}
+        float aa = brightness * (1.0 - sat);
+        float bb = brightness * (1.0 - sat * hue_frac);
+        float cc = brightness * (1.0 - sat * (1.0 - hue_frac));
 
-void pattern_greys(PIO pio, uint sm, uint len, uint t) {
-    uint max = 100; // let's not draw too much current!
-    t %= max;
-    for (uint i = 0; i < len; ++i) {
-        put_pixel(pio, sm, t * 0x10101);
-        if (++t >= max) t = 0;
+        switch (slice)
+        {
+        case 0:
+            red = brightness;
+            green = cc;
+            blue = aa;
+            break;
+        case 1:
+            red = bb;
+            green = brightness;
+            blue = aa;
+            break;
+        case 2:
+            red = aa;
+            green = brightness;
+            blue = cc;
+            break;
+        case 3:
+            red = aa;
+            green = bb;
+            blue = brightness;
+            break;
+        case 4:
+            red = cc;
+            green = aa;
+            blue = brightness;
+            break;
+        case 5:
+            red = brightness;
+            green = aa;
+            blue = bb;
+            break;
+        default:
+            red = 0.0;
+            green = 0.0;
+            blue = 0.0;
+            break;
+        }
     }
+
+    unsigned char ired = red * 255.0;
+    unsigned char igreen = green * 255.0;
+    unsigned char iblue = blue * 255.0;
+
+    wsColor c;
+    c.r = ired;
+    c.g = igreen;
+    c.b = iblue;
+    return c;
 }
 
-typedef void (*pattern)(PIO pio, uint sm, uint len, uint t);
-const struct {
-    pattern pat;
-    const char *name;
-} pattern_table[] = {
-        {pattern_snakes,  "Snakes!"},
-        {pattern_random,  "Random data"},
-        {pattern_sparkle, "Sparkles"},
-        {pattern_greys,   "Greys"},
-};
-
-int main() {
-    //set_sys_clock_48();
+int main()
+{
+    // set_sys_clock_48();
     stdio_init_all();
     printf("WS2812 Smoke Test, using pin %d\n", WS2812_PIN);
 
@@ -125,17 +169,26 @@ int main() {
 
     ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
 
-    int t = 0;
-    while (1) {
-        int pat = rand() % count_of(pattern_table);
-        int dir = (rand() >> 30) & 1 ? 1 : -1;
-        puts(pattern_table[pat].name);
-        puts(dir == 1 ? "(forward)" : "(backward)");
-        for (int i = 0; i < 1000; ++i) {
-            pattern_table[pat].pat(pio, sm, NUM_PIXELS, t);
-            sleep_ms(10);
-            t += dir;
+    float a = 0;
+    while (1)
+    {
+        wsColor colors[NUM_PIXELS] = {
+            HSBtoRGB(fmod(a, 360), 1.0, 0.1),
+            HSBtoRGB(fmod(a + 90, 360), 1.0, 0.1),
+            HSBtoRGB(fmod(a + 180, 360), 1.0, 0.1),
+            HSBtoRGB(fmod(a + 270, 360), 1.0, 0.1)};
+
+        uint8_t red[NUM_PIXELS] = {colors[0].r, colors[1].r, colors[2].r, colors[3].r};
+        uint8_t green[NUM_PIXELS] = {colors[0].g, colors[1].g, colors[2].g, colors[3].g};
+        uint8_t blue[NUM_PIXELS] = {colors[0].b, colors[1].b, colors[2].b, colors[3].b};
+
+        for (int i = 0; i < NUM_PIXELS; i++)
+        {
+            // put_pixel(pio, sm, urgbw_u32(red[i], green[i], blue[i], 0));
+            put_pixel(pio, sm, urgb_u32(red[i], green[i], blue[i]));
         }
+        a = fmodf(a + (360.0f / (5.0f * 100.0f)), 360.0f);
+        sleep_ms(1);
     }
 
     // This will free resources and unload our program
